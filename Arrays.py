92315bac12d747163107a45709fcd7c6a1f66099
Anvita <kumaranvita30@gmail.com>
2024-07-10 23:49:43 +0530

    init

---
+++ Arrays.py
@@ -0,0 +1,22 @@
+new_list = [1, 2, 3, 4, 5, 6]
+print(new_list)
+result = new_list[0]
+print(result)
+
+if 1 in new_list: print(True)
+
+for n in new_list:
+    if n==1:
+        print(True)
+        break
+
+numbers = []
+
+print(len(numbers))
+
+numbers.append(2)
+numbers.append(200)
+print(numbers)
+num2 = []
+num2.extend([4, 5, 6])
+print(num2)
